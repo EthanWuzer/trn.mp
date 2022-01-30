65b9a5a72b4f62e8064ff5152a1f947ea91fd549
Isaac Braun <isaacbraun2000@gmail.com>
2022-01-30 16:53:02 -0500

    Beggining Work on List Content/Styling

--- src/Home.js
+++ src/Home.js
@@ -10,39 +10,31 @@ import styled from "styled-components";
 import { formatRelative } from "date-fns";
 import mapStyles from "./styles/mapStyles";
 
-
 const libraries = ["places"];
 
-const mapContainerStyle = {
-  width: "60vw",
-  height: "100vh"
-};
-
-const options = {
-  styles: mapStyles
-};
-
-
 function Map(props) {
-    console.log(Object.entries(props.locations));
+    // console.log(Object.entries(props.locations));
 
     return (
       <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
         <GoogleMap 
-          mapContainerStyle={mapContainerStyle} 
+          mapContainerStyle={{
+            width: "70vw",
+            height: "85vh"
+          }}
           zoom={12} 
           center={props.center}
-          options={options}>
+          options={{styles: mapStyles}}>
           {props.locations.map(marker =>
           {
-            console.log("Marker: ", Object.entries(marker));
-            console.log("Maker Status: ", marker.status);
+            // console.log("Marker: ", Object.entries(marker));
+            // console.log("Maker Status: ", marker.status);
             return(
             <Marker 
               key={marker.id} 
               position ={{lat:marker.latitude, lng:marker.longitude}} 
               icon ={{
-                url: marker.status ? '/train-outline.svg' : '/logo192.png',
+                url: marker.status ? '/logo192.png' : '/train-outline.svg',
                 scaledSize: new window.google.maps.Size(30,30),
               }}
             />
@@ -77,7 +69,7 @@ function Home() {
             }
             setIsLoaded(true);
             setLocations(data);
-          },
+          }, 
           (error) => {
             setIsLoaded(true);
             setError(error);
@@ -90,20 +82,41 @@ function Home() {
   const handleClick = useCallback(() => {
     setCenterpoint({lat:33, lng:-84})
   });
+
   return(
       <HomeContainer>
-        <ListContainer>
-          {locations.map(intersection => {
-            return(
-              <IntersectionContainer onClick={(e) => handleClick()}>
-                <Header>{intersection.title}</Header>
-              </IntersectionContainer>
-            )
-          })}
-        </ListContainer>
-        <MapContainer>
-          <Map center={centerpoint} locations={locations}/>
-        </MapContainer>
+        <Header>trn.mp</Header>
+        <InnerContainer>
+          <ListContainer>
+            <ListHeader>
+              intersections.
+            </ListHeader>
+            {locations.map(intersection => {
+              // console.log("Intersection: ", intersection);
+              return (
+                <IntersectionContainer
+                  key={intersection.id}
+                  onClick={(e) => handleClick()}
+                >
+                  <IntersectionLeft>
+                    <IntersectionTitle>{intersection.title}</IntersectionTitle>
+                  </IntersectionLeft>
+                  <IntersectionRight>
+                    {intersection.status ? 
+                      <StatusIcon alt="Blocked Icon" src="/logo192.png" />
+                      :
+                      <StatusIcon alt="Clear Icon" src="/train-outline.svg" />
+                    }
+                    <StatusText>{intersection.status ? 'Blocked' : 'Clear'}</StatusText>
+                  </IntersectionRight>
+                </IntersectionContainer>
+              )
+            })}
+          </ListContainer>
+          <MapContainer>
+            <Map center={centerpoint} locations={locations}/>
+          </MapContainer>
+        </InnerContainer>
       </HomeContainer>
   );
 }
@@ -112,31 +125,77 @@ export default Home;
 
 const HomeContainer = styled.div`
   display: flex;
-  width: 100vw;
+  flex-direction: column;
+`
+const Header = styled.h1`
+  margin-left: 25px;
+`
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `
 
-const IntersectionContainer = styled.div`
-  width: 90%;
-  height: 200px;
-  background-color: #fff;
-  margin: auto;
 
-  
+const ListContainer = styled.div`
+  width: 100%;
+  height: 85vh;
+  margin: 0 25px 0 0;
+  background-color: #eeeeee;
+  border-radius: 0 5px 5px 0;
+
+  overflow: scroll;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+
+  ::-webkit-scrollbar {
+    display: none; /* Chrome, Safari and Opera */
+  }
+`
+const ListHeader = styled.h3`
+  margin: 25px 0 0 25px;
+`
+
+
+const IntersectionContainer = styled.div`
+  height: 150px;
+  background-color: #fff;
+  margin: 25px 25px 0 25px;
+  display: flex;
+  flex-direction: row;
+  border-radius: 5px;
+
   :hover {
     cursor: pointer;
   }
 `
-const Header = styled.h1`
+const IntersectionLeft = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+const IntersectionRight = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+`
+const IntersectionTitle = styled.h2`
 
 `
+const StatusIcon = styled.img`
+  width: 50px;
+  height: auto;
+
+`
+const StatusText = styled.h2`
+  
+`
+
 
 const MapContainer = styled.div`
+  border-radius: 5px 0 0 5px;
   position: relative;
-  width: 60vw;
 `
-
-const ListContainer = styled.div`
-  width: 40%;
-  background-color: #eeeeee;
-`
-
